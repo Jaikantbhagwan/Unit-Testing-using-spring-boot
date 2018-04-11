5e447d96c997913085c06afac778f32344387f37
Jaikant S <jaikant_singh@apple.com>
2018-04-11 17:06:00 +0530

    List Junit test case

---
+++ unit-testing/src/test/java/com/jaikant/unittesting/unittesting/business/ListMockTest.java
@@ -0,0 +1,47 @@
+
+
+package com.jaikant.unittesting.unittesting.business;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Test;
+
+
+public class ListMockTest {
+
+    List mock = mock(List.class);
+
+    @Test
+    public void size_basic() {
+        when(mock.size()).thenReturn(5);
+        assertEquals(5, mock.size());
+    }
+
+    @Test
+    public void returnDifferentValues() {
+        when(mock.size()).thenReturn(5).thenReturn(10);
+        assertEquals(5, mock.size());
+        assertEquals(10, mock.size());
+    }
+
+    @Test
+    public void returnWithParameters() {
+        when(mock.get(0)).thenReturn("Jai");
+        assertEquals("Jai", mock.get(0));
+        assertEquals(null, mock.get(1));
+    }
+
+    @Test
+    public void returnWithGenericParameters() {
+        when(mock.get(anyInt())).thenReturn("Jai");
+        assertEquals("Jai", mock.get(0));
+        assertEquals("Jai", mock.get(1));
+    }
+
+}
