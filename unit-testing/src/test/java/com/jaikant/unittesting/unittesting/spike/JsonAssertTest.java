413ebea4077f1ded396a147e2071053f76526a8f
acha47 <jaikantjk@gmail.com>
2018-04-14 01:01:27 +0530

    Added more test cases with JSON

--- unit-testing/src/test/java/com/jaikant/unittesting/unittesting/spike/JsonAssertTest.java
+++ unit-testing/src/test/java/com/jaikant/unittesting/unittesting/spike/JsonAssertTest.java
@@ -9,8 +9,28 @@ public class JsonAssertTest {
 	String actualResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
 	
 	@Test
-	public void jsonAssert() throws JSONException {
-		String expectedResponse = "{\"id\":1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+	public void jsonAssert_StrictTrue_ExactMatch_ExceptWhiteSpaces() throws JSONException {
+		String expectedResponse = "{\"id\": 1,\"name\":\"Ball\",\"price\":10,\"quantity\":100}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+	}
+	
+	@Test
+	public void jsonAssert_StrictFalse() throws JSONException {
+		String expectedResponse = "{\"id\":1,\"name\":\"Ball\"}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+	}
+	
+	@Test
+	public void jsonAssert_WithoutEscapeCharacters() throws JSONException {
+		String expectedResponse = "{id:1,name:Ball}";
+		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
+	}
+	
+	@Test
+	public void jsonAssert_USeEscapeCharacters_When_String_Has_Space() throws JSONException {
+		String actualResponse = "{\"id\":1,\"name\":\"Ball 2\",\"price\":10,\"quantity\":100}";
+		
+		String expectedResponse = "{id:1,name:\"Ball 2\"}";
 		JSONAssert.assertEquals(expectedResponse, actualResponse, false);
 	}
 
